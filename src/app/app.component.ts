381b49dfca059ce6be3597d806e9817ce30a7ecf
jaredDelao <jaredhm07@gmail.com>
2021-02-08 17:16:05 -0600

    Form array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'formControls';
+export class AppComponent implements OnInit {
+
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    
+
+    this.form = this.fb.group({
+      nombre: [null],
+      arreglo: this.fb.array([])
+    })
+
+  }
+
+
+  agregarTelefonoCorreo() {
+
+    const arreglo = this.form.get('arreglo') as FormArray;
+
+    const grupo = this.fb.group({
+      telefono: [null],
+      correo: [null]
+    })
+
+    arreglo.push(grupo);
+
+  }
+
+
+  borrarGrupo(i: number) {
+    const arreglo = this.form.get('arreglo') as FormArray;
+    arreglo.removeAt(i);
+  }
+  
+
+
 }
